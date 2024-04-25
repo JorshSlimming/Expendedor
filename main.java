b10aedbbcc2b22d7fe869abaafabb0cc8ea4b6a0
JorshSlimming <slimmingjorge@gmail.com>
2024-04-25 01:09:38 -0400

    Update Main
    Se agregan casos extras de prueba

--- main.java
+++ main.java
@@ -14,12 +14,21 @@ public class main {
         System.out.println("Comprador 2: Producto adquirido: " + comprador2.queConsumiste() + ", vuelto: " + comprador2.cuantoVuelto());
 
         // Intentar comprar un snack con moneda de $100 y depósito 2 (debería lanzar una PagoIncorrectoException)
-        Comprador comprador3 = new Comprador(moneda100, 2, expendedor);
+        Comprador comprador3 = new Comprador(moneda100, 2, expendedor); // Error: PagoIncorrectoException
 
         // Intentar comprar un producto con moneda null y depósito 1 (debería lanzar una PagoIncorrectoException)
-        Comprador comprador4 = new Comprador(null, 1, expendedor);
+        Comprador comprador4 = new Comprador(null, 1, expendedor); // Error: PagoIncorrectoException
 
         // Intentar comprar un producto con depósito incorrecto (debería lanzar una NoHayProductoException)
-        Comprador comprador5 = new Comprador(moneda1000, 5, expendedor);
+        Comprador comprador5 = new Comprador(moneda1000, 5, expendedor); // Error: NoHayProductoException
+
+        // Intentar comprar un producto con una moneda de valor suficiente pero depósito inexistente (debería lanzar una NoHayProductoException)
+        Comprador comprador6 = new Comprador(moneda1000, 20, expendedor); // Error: NoHayProductoException
+
+        // Intentar comprar un producto con una moneda de valor insuficiente y depósito inexistente (debería lanzar una NoHayProductoException)
+        Comprador comprador7 = new Comprador(moneda100, 20, expendedor); // Error: NoHayProductoException
+
+        // Intentar comprar un producto con una moneda null y depósito inexistente (debería lanzar una PagoIncorrectoException)
+        Comprador comprador8 = new Comprador(null, 20, expendedor); // Error: PagoIncorrectoException
     }
 }
